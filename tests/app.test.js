2afaa2f42a2197b954656f50c4e17ba5f1dec8c3
Tiffany Bowers <40266916+tiffanybowers@users.noreply.github.com>
2022-11-23 01:19:25 +0200

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,16 +1,8 @@
-// const { default: test } = require("node:test")
-
-// const { exportAllDeclaration } = require("@babel/types");
-
 test('Should display all users and their followers', async () => {
     const data = await userData;
     expect(data).toBe(fs.readFile("user.txt", "ascii"));
 });
 
-test('Hello world', () => {
+//test('Hello world', () => {
 
-})
-
-test('Second Hello World', () => {
-    throw new Error('Failure!')
 })
