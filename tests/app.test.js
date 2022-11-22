c32a95a793cedda3e4df4bf5257ac420df3a8d00
Tiffany Bowers <40266916+tiffanybowers@users.noreply.github.com>
2022-11-23 01:35:57 +0200

    Delete tests directory

--- tests/app.test.js
+++
@@ -1,8 +0,0 @@
-test('Should display all users and their followers', async () => {
-    const data = await userData;
-    expect(data).toBe(fs.readFile("user.txt", "ascii"));
-});
-
-//test('Hello world', () => {
-
-})
